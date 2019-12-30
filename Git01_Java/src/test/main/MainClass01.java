48ec5fc06b3979dd5323a2129fe6d63bf23a77b4
parkseulji <parkseulji112@gmail.com>
2019-12-30 10:40:54 +0900

    클래스 새로 만듬

---
+++ Git01_Java/src/test/main/MainClass01.java
@@ -0,0 +1,10 @@
+package test.main;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		System.out.println("kimgura");
+		System.out.println("monkey");
+		System.out.println("lab1브랜치에서 작업함");
+		System.out.println("lab1브랜치에서 작업함2");
+	}
+}
